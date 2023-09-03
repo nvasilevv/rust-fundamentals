03e35f4eb992a575bbf282d437a637e7fac5727f
niki <nikolay.vasilev@2iqresearch.com>
2023-09-03 18:36:18 +0300

    feat(main): adding some unused functions they show more info about how to work with string slices and String

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,26 @@ fn main() {
     location_tuple.0, location_tuple.1, location_tuple.2);
     println!("Location name: {}, latitude: {}, longitude: {}",
     name, latitude, longitude);
+    concat_str_slice();
+}
+
+
+fn convert_str_slice() {
+    let person_name_string = String::from("Donald Mallard");
+    let person_name_slice = &person_name_string; //dereferencing a variable and actually points to the heap memory address
+    let person_name_slice2 = person_name_string.as_str(); //& and * are the 2 characters which deal with memory pointers
+}
+
+
+fn concat_str_slice() {
+    let duck = "Duck";
+    let airlines = "Airlines";
+    let airline_name = [duck, " ", airlines].concat(); //note that the concat produces a String out of string slices
+    println!("{}", airline_name); //exclamation mark denotes macros
+    println!("{}", format!("{} {}", duck, airlines));
+    let mut slogan = String::new();
+    slogan.push_str("We hit the ground");
+    slogan.push(' '); //note that push expects a char which is passed with single quotes. Otherwise, compile error
+    slogan = slogan + "every time";
+    println!("{}", slogan);
 }
